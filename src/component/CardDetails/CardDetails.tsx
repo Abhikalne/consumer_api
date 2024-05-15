aa8810f6b28dd06ad9aa81c3a394b0ebc5b8f0fe
Abhijit Kalne <abhikalne1@gmail.com>
2024-05-15 20:28:24 +0530

    fix issuesv2

--- src/component/CardDetails/CardDetails.tsx
+++ src/component/CardDetails/CardDetails.tsx
@@ -9,82 +9,82 @@ import { cardDetailsProps, cardType } from "../../common/type";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 function CardDetails({ items }: cardDetailsProps) {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { id } = useParams();
-  const path = location.pathname.split("/")[1];
-  const { cardDetails, loading, error } = useAppSelector(
-    (state: RootState) => state.cardDetail
-  );
-  const [slideIndex, setslideIndex] = useState(0);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { id } = useParams();
+    const path = location.pathname.split("/")[1];
+    const { cardDetails, loading, error } = useAppSelector(
+        (state: RootState) => state.cardDetail
+    );
+    const [slideIndex, setslideIndex] = useState(0);
 
-  const handlePrev = () => {
-    setslideIndex((prev) => (prev === 0 ? items.length - 1 : prev - 1));
-  };
-  const handleNext = () => {
-    setslideIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
-  };
-  useEffect(() => {
-    dispatch(getData_api(items[slideIndex].url));
-    navigate("/" + path + "/" + items[slideIndex].uid);
+    const handlePrev = () => {
+        setslideIndex((prev) => (prev === 0 ? items.length - 1 : prev - 1));
+    };
+    const handleNext = () => {
+        setslideIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
+    };
+    useEffect(() => {
+        dispatch(getData_api(items[slideIndex].url));
+        navigate("/" + path + "/" + items[slideIndex].uid);
     // eslint-disable-next-line
   }, [dispatch, slideIndex]);
 
-  return (
-    <div className="card-parent">
-      {" "}
-      <div className="arrow-button" onClick={handlePrev}>
-        {" "}
-        <FaAngleLeft data-testid="btn-prev" />{" "}
-      </div>{" "}
-      {items.map((ele: cardType, ind: number) => {
-        return (
-          <div
-            key={ind}
-            className={slideIndex === ind ? "slide active-slide" : "slide"}
-          >
+    return (
+        <div className="card-parent">
             {" "}
-            <h3>{ele.name}</h3>{" "}
-            <div>
-              {" "}
-              {loading ? (
-                <SpinningCircles
-                  className="loading"
-                  data-testid="loading"
-                  speed={1.5}
-                />
-              ) : error ? (
-                error
-              ) : (
-                cardDetails.uid === id &&
-                Object.entries(cardDetails.properties)?.map(
-                  // eslint-disable-next-line
-                  ([key, value]: any) => (
-                    <h5 key={key + value}>
-                      {" "}
-                      {key.replace("_", " ").toUpperCase()}:-{" "}
-                      {Array.isArray(value)
-                        ? value.map((itm: string) => (
-                            <pre key={itm} style={{ fontSize: "0.8rem" }}>
-                              {" "}
-                              {itm}{" "}
-                            </pre>
-                          ))
-                        : value}{" "}
-                    </h5>
-                  )
-                )
-              )}{" "}
+            <div className="arrow-button" onClick={handlePrev}>
+                {" "}
+                <FaAngleLeft data-testid="btn-prev" />{" "}
             </div>{" "}
-          </div>
-        );
-      })}{" "}
-      <div className="arrow-button" onClick={handleNext}>
-        {" "}
-        <FaAngleRight size={16} data-testid="btn-next" />{" "}
-      </div>{" "}
-    </div>
-  );
+            {items.map((ele: cardType, ind: number) => {
+                return (
+                    <div
+                        key={ind}
+                        className={slideIndex === ind ? "slide active-slide" : "slide"}
+                    >
+                        {" "}
+                        <h3>{ele.name}</h3>{" "}
+                        <div>
+                            {" "}
+                            {loading ? (
+                                <SpinningCircles
+                                    className="loading"
+                                    data-testid="loading"
+                                    speed={1.5}
+                                />
+                            ) : error ? (
+                                error
+                            ) : (
+                                cardDetails.uid === id &&
+                Object.entries(cardDetails.properties)?.map(
+                    // eslint-disable-next-line
+                  ([key, value]: any) => (
+                        <h5 key={key + value}>
+                            {" "}
+                            {key.replace("_", " ").toUpperCase()}:-{" "}
+                            {Array.isArray(value)
+                                ? value.map((itm: string) => (
+                                    <pre key={itm} style={{ fontSize: "0.8rem" }}>
+                                        {" "}
+                                        {itm}{" "}
+                                    </pre>
+                                ))
+                                : value}{" "}
+                        </h5>
+                    )
+                )
+                            )}{" "}
+                        </div>{" "}
+                    </div>
+                );
+            })}{" "}
+            <div className="arrow-button" onClick={handleNext}>
+                {" "}
+                <FaAngleRight size={16} data-testid="btn-next" />{" "}
+            </div>{" "}
+        </div>
+    );
 }
 export default React.memo(CardDetails);
